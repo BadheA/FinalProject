d0cf11040775e1254c4f50133893f6db2b989dcf
BadheA <badhe.ab@northeastern.edu>
2022-12-05 11:55:48 -0500

    Lab Enterprise

---
+++ src/Buss/Enterprise/LaboratoryEnt.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Buss.Enterprise;
+
+import Buss.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Abhishek Badhe
+ */
+public class LaboratoryEnt  extends Enterprise{
+     public LaboratoryEnt(String name){
+        super(name,EnterpriseType.Laboratory);
+    }
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+}
