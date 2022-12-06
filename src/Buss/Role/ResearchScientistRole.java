9162078adef638b306aaaee55a45251fcd63cb6b
BadheA <badhe.ab@northeastern.edu>
2022-12-06 11:20:03 -0500

    ScientistRole

---
+++ src/Buss/Role/ResearchScientistRole.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Buss.Role;
+
+import Bus.EcoSystem;
+import Buss.Enterprise.Enterprise;
+import Buss.Organiztion.Organization;
+import Buss.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.ResearchScientistRole.ResearchScientistWorkAreaJPanel;
+
+/**
+ *
+ * @author Abhishek Badhe
+ */
+public class ResearchScientistRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new ResearchScientistWorkAreaJPanel(userProcessContainer,enterprise, account, organization, business);
+    }
+    
+}
