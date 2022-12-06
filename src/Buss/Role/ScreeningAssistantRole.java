5099acd08ecf9db50f70513ced1c89a4002f3090
BadheA <badhe.ab@northeastern.edu>
2022-12-06 12:00:24 -0500

    Screening Asst

---
+++ src/Buss/Role/ScreeningAssistantRole.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Buss.Role;
+
+import Bus.EcoSystem;
+import Buss.Enterprise.Enterprise;
+import Buss.Organiztion.Organization;
+import Buss.UserAccount.UserAccount;
+import userinterface.LabAssistantRole.LabAssistantWorkAreaJPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Abhishek Badhe
+ */
+public class ScreeningAssistantRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new LabAssistantWorkAreaJPanel(userProcessContainer, account, organization, business);
+    }
+    
+}
